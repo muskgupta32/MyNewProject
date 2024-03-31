cb28c099fe80b0afd4c121daa432ca33ff2c306a
muskgupta32 <muskgupta32@gmail.com>
2024-03-31 13:59:35 +0530

    fifth commit

--- appsail-nodejs/index.js
+++ appsail-nodejs/index.js
@@ -3,7 +3,7 @@ const app = Express();
 const port = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 9000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!demo')
+  res.send('Hello World!')
 });
 
 app.listen(port, () => {
